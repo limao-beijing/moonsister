81269a6f23b2a35cfb673c33609241b4050c1d7a
jb <a>
2016-08-27 22:55:23 +0800

    搜索

--- app/src/main/java/com/moonsister/tcjy/home/widget/SearchFragment.java
+++ app/src/main/java/com/moonsister/tcjy/home/widget/SearchFragment.java
@@ -3,8 +3,10 @@ package com.moonsister.tcjy.home.widget;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupWindow;
 
 import com.moonsister.tcjy.R;
 import com.moonsister.tcjy.base.BaseFragment;
@@ -12,10 +14,11 @@ import com.moonsister.tcjy.base.BaseFragment;
 /**
  * Created by jb on 2016/8/26.
  */
-public class SearchFragment extends BaseFragment {
+public class SearchFragment extends BaseFragment implements SearchHeaderFragment.onSearchHeaderFragmentListener {
 
     private SearchHeaderFragment searchHeadFragment;
     private SearchContentFragment searchContentFragment;
+    private PopupWindow popupWindow;
 
     public static Fragment newInstance() {
         return new SearchFragment();
@@ -30,9 +33,53 @@ public class SearchFragment extends BaseFragment {
     protected void initData() {
         searchHeadFragment = SearchHeaderFragment.newInstance();
         replaceFramgent(searchHeadFragment, R.id.fl_search_head);
+        searchHeadFragment.setSearchHeaderFragmentListener(this);
         searchContentFragment = SearchContentFragment.newInstance();
         replaceFramgent(searchContentFragment, R.id.fl_search_content);
+        getPopupWindow();
     }
 
 
+    @Override
+    public void onChange(String key) {
+
+    }
+
+    /**
+     * 创建PopupWindow
+     */
+    protected void initPopuptWindow() {
+        // TODO Auto-generated method stub
+        // 获取自定义布局文件activity_popupwindow_left.xml的视图
+        View popupWindow_view = getActivity().getLayoutInflater().inflate(R.layout.popupwindow_search, null,
+                false);
+        // 创建PopupWindow实例,200,LayoutParams.MATCH_PARENT分别是宽度和高度
+        popupWindow = new PopupWindow(popupWindow_view, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, true);
+        // 设置动画效果
+//        popupWindow.setAnimationStyle(R.style.AnimationFade);
+        // 点击其他地方消失
+        popupWindow_view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                if (popupWindow != null && popupWindow.isShowing()) {
+                    popupWindow.dismiss();
+                    popupWindow = null;
+                }
+                return false;
+            }
+        });
+    }
+
+    /***
+     * 获取PopupWindow实例
+     */
+    private void getPopupWindow() {
+        if (null != popupWindow) {
+            popupWindow.dismiss();
+            return;
+        } else {
+            initPopuptWindow();
+        }
+    }
 }
--- app/src/main/java/com/moonsister/tcjy/home/widget/SearchHeaderFragment.java
+++ app/src/main/java/com/moonsister/tcjy/home/widget/SearchHeaderFragment.java
@@ -1,17 +1,31 @@
 package com.moonsister.tcjy.home.widget;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.moonsister.tcjy.R;
 import com.moonsister.tcjy.base.BaseFragment;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by jb on 2016/8/26.
  */
-public class SearchHeaderFragment extends BaseFragment {
+public class SearchHeaderFragment extends BaseFragment implements TextWatcher {
+    @Bind(R.id.tv_cancel)
+    TextView tvCancel;
+    @Bind(R.id.et_content)
+    EditText etContent;
+    private onSearchHeaderFragmentListener listener;
+
     public static SearchHeaderFragment newInstance() {
         return new SearchHeaderFragment();
     }
@@ -23,6 +37,47 @@ public class SearchHeaderFragment extends BaseFragment {
 
     @Override
     protected void initData() {
+        etContent.addTextChangedListener(this);
+    }
 
+
+    @OnClick({R.id.tv_cancel, R.id.iv_search_cancel})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_cancel:
+                getActivity().finish();
+                break;
+            case R.id.iv_search_cancel:
+                etContent.setText("");
+                break;
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if (listener != null) {
+            listener.onChange(s.toString());
+        }
+    }
+
+    /**
+     * 文字改变
+     */
+    public interface onSearchHeaderFragmentListener {
+        void onChange(String key);
+    }
+
+    public void setSearchHeaderFragmentListener(onSearchHeaderFragmentListener listener) {
+        this.listener = listener;
     }
 }
