6b5ea2de60066d69dd2674a08cbe4d72a3b6fa88
wubaoping <3383906707@qq.com>
2016-09-10 17:25:02 +0800

    wubaoping

---
+++ app/src/main/java/com/moonsister/tcjy/my/model/PersonalReviseActivityModelImpl.java
@@ -0,0 +1,16 @@
+package com.moonsister.tcjy.my.model;
+
+import com.moonsister.tcjy.base.BaseIModel;
+import com.moonsister.tcjy.bean.PersonalMessageBean;
+import com.moonsister.tcjy.bean.PersonalReviseMessageBean;
+
+/**
+ * Created by x on 2016/9/10.
+ */
+public class PersonalReviseActivityModelImpl implements PersonalReviseActivityModel {
+
+    @Override
+    public void loadpersonalData(int uid, onLoadDateSingleListener<PersonalReviseMessageBean> listener) {
+
+    }
+}
